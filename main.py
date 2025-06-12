fc11c6eff75ccbef0906b8895a5a056a2f544157
ali zobeid <alizobeid1384@gmail.com>
2025-06-12 17:39:38 +0330

    develop web scraping app to get IPs from cable.ayra.ch

---
+++ main.py
@@ -0,0 +1,55 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+from time import sleep
+
+
+class MyBrowser:
+    def __init__(self):
+        self.browser = webdriver.Firefox()
+        self.result_page = None
+
+    def open_page(self, country_code):
+        self.browser.get(f"https://cable.ayra.ch/ip/?c={country_code}")
+        self.page = self.browser.page_source
+
+    def get_real_time_page_source(self):
+        return self.browser.page_source
+
+    @property
+    def page(self):
+        return self.result_page
+
+    @page.setter
+    def page(self, source):
+        self.result_page = source
+
+    def check_next_page(self):
+        value = self.browser.find_element(By.ID, "DataTables_Table_0_next").get_attribute(
+            "class"
+        )
+        return "disable" not in value
+
+    def go_to_next_page(self):
+        if self.check_next_page():
+            self.browser.find_element(By.CLASS_NAME, "next").click()
+            return True
+
+
+firefox = MyBrowser()
+firefox.open_page("IR")
+while True:
+    saved_page = firefox.result_page
+    if saved_page == firefox.get_real_time_page_source():
+        table = BeautifulSoup(saved_page,"html.parser").find_all("table")[0].get_text()
+        with open("IR_IPs.txt","a") as TABLE: 
+            TABLE.write(table)
+        if firefox.check_next_page():
+            print("im_hear_go_to_next")
+            firefox.go_to_next_page()
+        else:
+            print("no more page")
+            break
+    else:
+        firefox.page = firefox.get_real_time_page_source()
+firefox.browser.quit()
